75a8cebd00d9f5b55408895dd0c0b50d498cd35a
Jeff Casimir <jeff@casimircreative.com>
2014-01-13 11:39:03 -0700

    Implemented dependency inversion to mock out API

--- lib/article_feed/feed.rb
+++ lib/article_feed/feed.rb
@@ -3,17 +3,24 @@ require 'json'
 
 module ArticleFeed
   class Feed
+    attr_reader :data_store
+
+    def initialize(input_data_store = nil)
+      @data_store = (input_data_store || ArticleStore).new
+    end
+
     def first_article
-      ArticleStore.new.first
+      data_store.first
     end
   end
 
   class Article
-    attr_reader :title, :id
+    attr_reader :title, :id, :data_source
 
-    def initialize(input)
+    def initialize(input, input_data_source)
       @title = input["title"]
       @id    = input["id"]
+      @data_source = input_data_source
     end
 
     def word_count
@@ -21,22 +28,20 @@ module ArticleFeed
     end
 
     def body
-      ArticleStore.new.body_for(id)
+      data_source.body_for(id)
     end
   end
 
   class ArticleStore
-    def url_root
-      "http://localhost:3000/api/v1/"
-    end
+    attr_reader :data_source
 
-    def extension
-      ".json"
+    def initialize(input_data_source = ArticleDataSource)
+      @data_source = input_data_source.new
     end
 
     def first
       data = fetch_data("articles")
-      Article.new(data.first)
+      Article.new(data.first, self)
     end
 
     def body_for(id)
@@ -45,7 +50,21 @@ module ArticleFeed
     end
 
     def fetch_data(path)
+      data_source.fetch( path )      
+    end
+  end
+
+  class ArticleDataSource
+    def fetch(path)
       JSON.parse( Faraday.get(url_root + path + extension).body )
+    end
+
+    def url_root
+      "http://localhost:3000/api/v1/"
+    end
+
+    def extension
+      ".json"
     end
   end
 end
--- test/feed_test.rb
+++ test/feed_test.rb
@@ -8,14 +8,36 @@ class FeedTest < Minitest::Test
     assert ArticleFeed::Feed
   end
 
+  class FakeDataSource
+    def fetch(path)
+      if path == "articles"
+        JSON.parse( File.read('./test/fixtures/articles.json'))
+      else
+        JSON.parse( File.read('./test/fixtures/article.json'))
+      end
+    end
+  end
+
+  class FakeArticleStore < ArticleFeed::ArticleStore
+    def data_source
+      FakeDataSource.new
+    end
+  end
+
+  def test_first_article_is_first_in_the_data_store
+    feed = ArticleFeed::Feed.new( FakeArticleStore )
+    expected = "Qui Ipsa Maiores Iusto Eligendi Aut 0"
+    assert_equal expected, feed.first_article.title
+  end
+
   def test_it_finds_the_first_article
     expected_title = "Qui Ipsa Maiores Iusto Eligendi Aut 0"
-    feed = ArticleFeed::Feed.new
+    feed = ArticleFeed::Feed.new(FakeArticleStore)
     assert_equal expected_title, feed.first_article.title
   end
 
   def test_it_finds_the_first_article_word_count
-    feed = ArticleFeed::Feed.new
+    feed = ArticleFeed::Feed.new(FakeArticleStore)
     first_article = feed.first_article
     assert_equal 26, first_article.word_count
   end
